d9b44d8d55589e179cd0e8b47597e34fbcbd0b9e
DDDZZZ <dylee0568@gmail.com>
2021-11-09 20:44:47 +0900

    DY_1109_2 register(mno null 수정 필요)

---
+++ src/main/java/com/ds/antddun/controller/MemberController.java
@@ -0,0 +1,26 @@
+package com.ds.antddun.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@Log4j2
+public class MemberController {
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        log.info("LOGIN >>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        return "/member/login";
+    }
+
+    @GetMapping("/member/mypage")
+    public String wallet() {
+        return "/member/mypage";
+    }
+//    @PutMapping("member/updateMember")
+//    public
+}
