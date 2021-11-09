e369597f7977332ad9e6d8799cc47ec8c5006185
DDDZZZ <dylee0568@gmail.com>
2021-11-09 23:20:50 +0900

    DY_1109_3 register 구현(유효성 검사 필요)

--- src/test/java/com/ds/antddun/service/SosoJobServiceImplTest.java
+++ src/test/java/com/ds/antddun/service/SosoJobServiceImplTest.java
@@ -17,23 +17,23 @@ class SosoJobServiceImplTest {
     @Autowired
     private SosoJobService sosoJobService;
 
-    @Test
-    void register() {
-
-
-        IntStream.rangeClosed(1, 50).forEach(i -> {
-
-                SosoBoardDTO sosoBoardDTO = SosoBoardDTO.builder()
-                        .title("홈페이지 제작합니다" + i)
-                        .category(i < 25 ? "IT" : "마케팅")
-                        .content("홈페이지 제작" + i)
-                        .ddun(10000)
-                        .mno(1L)
-                        .build();
-
-            System.out.println(sosoJobService.register(sosoBoardDTO));
-        });
-    }
+//    @Test
+//    void register() {
+//
+//
+//        IntStream.rangeClosed(1, 50).forEach(i -> {
+//
+//                SosoBoardDTO sosoBoardDTO = SosoBoardDTO.builder()
+//                        .title("홈페이지 제작합니다" + i)
+//                        .category(i < 25 ? "IT" : "마케팅")
+//                        .content("홈페이지 제작" + i)
+//                        .ddun(10000)
+//                        .mno(1L)
+//                        .build();
+//
+////            System.out.println(sosoJobService.register(sosoBoardDTO));
+//        });
+//    }
 
 //    @Test
 //    public void testRead() {
