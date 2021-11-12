6ac52b6322296046e14c258bb64010c85b1d25a2
danbi-park <86641773+danbi-park@users.noreply.github.com>
2021-11-12 15:17:47 +0900

    PDB_1112
    - 멤버만 글쓰기 허용, 게시글 등록 후 리스트로 이동
    - 로그인 후 이전 요청 주소로 이동 (Handler 추가)
    - index, basic에서 정보게시판으로 가는 URL 수정
    - login 버튼 수정

--- src/main/java/com/ds/antddun/controller/QnaBoardController.java
+++ src/main/java/com/ds/antddun/controller/QnaBoardController.java
@@ -2,12 +2,13 @@ package com.ds.antddun.controller;
 
 import com.ds.antddun.config.auth.PrincipalDetails;
 import com.ds.antddun.dto.QnaBoardDTO;
-import com.ds.antddun.entity.JobList;
+import com.ds.antddun.entity.QnaBoard;
+import com.ds.antddun.repository.QnaBoardRepository;
 import com.ds.antddun.service.JobListService;
 import com.ds.antddun.service.QnaService;
 import lombok.extern.log4j.Log4j2;
-import net.bytebuddy.TypeCache;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -16,16 +17,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Log4j2
-@RequestMapping("/member")
+//@RequestMapping("/member") 이건 빼버리고
 @SessionAttributes("member")
 public class QnaBoardController {
 
@@ -33,53 +31,48 @@ public class QnaBoardController {
     private JobListService jobListService;
 
     @Autowired
+    private QnaBoardRepository repository;
+
+    @Autowired
     private QnaService qnaService;
 
 
-//    //리스트로 값을 보냄
-//    @GetMapping("/qna/list")
-//    public String list(Model model) {
-//        //직무 카테고리를 보냄
-//        List<JobList> jobListDTOList = jobListService.getList();
-//        model.addAttribute("jobList", jobListDTOList);
-//        //게시글 데이터를 보냄
-//        List<QnaBoardDTO> boardDTOList = qnaService.getBoardList();
-//        model.addAttribute("postList", boardDTOList);
-//        return "/qna/list";
-//    }
-
-    //리스트로 값을 보냄
-    @GetMapping("/qna/list")
-    public String lists(Model model, @PageableDefault(size = 6, sort = "regDate", direction = Sort.Direction.DESC) Pageable pageable) {
-        model.addAttribute("postList", qnaService.getList(pageable));
-        return "/qna/list";
-    }
-
-
-    //권한 확인
-    @GetMapping("/qna/register")
-    public String register(QnaBoardDTO qnaBoardDTO, HttpSession httpSession, HttpServletRequest request) {
-        httpSession = request.getSession();
-        log.info("httpSession>>>>>>" + httpSession.getId());
-        return "/qna/register";
+    //게시물 작성
+    @GetMapping("/member/qna/registerForm") //여기서는 principal로 한 번 더 확인
+    public String register(QnaBoardDTO qnaBoardDTO, @AuthenticationPrincipal PrincipalDetails principal) {
+        if (principal == null) {
+            System.out.println("멤버권한이 없음");
+            return "redirect:/login";
+        } else {
+            log.info("principal.getMember())" + principal.getMember());
+            return "/member/qna/registerForm"; //여기도 멤버를 붙여야하나? 한 번 더 확인하는
+        }
     }
 
     //글 등록
-    @PostMapping("/qna/confirm")
-    public String register(QnaBoardDTO qnaBoardDTO, @AuthenticationPrincipal PrincipalDetails principal) {
+    @PostMapping("/member/qna/register")
+    public ModelAndView register(QnaBoardDTO qnaBoardDTO, Model model , @AuthenticationPrincipal PrincipalDetails principal) {
         qnaService.register(qnaBoardDTO, principal.getMember());
-        log.info("REGISTER>>>>>>"+qnaBoardDTO+"/"+principal.getMember());
+        ModelAndView mav = new ModelAndView("redirect:/qna/list");
+        return mav;
+    }
+
+    //리스트로 값을 보냄(페이징 임시)
+    @GetMapping("/qna/list") //리스트는 회원들이 볼 수 있으니까 /member 안붙임
+    public String lists(Model model, @PageableDefault(size = 6, sort = "regDate", direction = Sort.Direction.DESC) Pageable pageable,
+                        RedirectAttributes redirect) {
+        model.addAttribute("postList", qnaService.getList(pageable));
+        redirect.addFlashAttribute("postList", qnaService.getList(pageable));
         return "/qna/list";
     }
 
+
+
     //게시판 조회
-    @GetMapping("/qna/read")
+    @GetMapping("/member/qna/read")
     public String read(){
-        return "board/view";
+        return "/member/qna/read";
     }
-
-
-
 
 
 }
