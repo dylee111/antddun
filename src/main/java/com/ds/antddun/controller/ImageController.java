7c33e0ebb7087ece307a2501c110faa13f2cd472
Kimgeunhye <85294826+Kimgeunhye@users.noreply.github.com>
2021-11-12 18:53:06 +0900

    1112 이미지 업로드 진행 중

--- src/main/java/com/ds/antddun/controller/ImageController.java
+++ src/main/java/com/ds/antddun/controller/ImageController.java
@@ -2,37 +2,37 @@ package com.ds.antddun.controller;
 
 import com.ds.antddun.entity.UploadFile;
 import com.ds.antddun.service.ImageService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
+@RequestMapping("/antddun")
+@Log4j2
 public class ImageController {
 
 	@Autowired
 	ImageService imageService;
-	
+
 	@Autowired
 	ResourceLoader resourceLoader;
-	
+
 	@PostMapping("/image")
 	public ResponseEntity<?> imageUpload(@RequestParam("file") MultipartFile file) {
 		try {
 			UploadFile uploadFile = imageService.store(file);
-			return ResponseEntity.ok().body("/image/" + uploadFile.getImageNo());
+			return ResponseEntity.ok().body("/image/" + uploadFile.getInum());
 		} catch(Exception e) {
 			e.printStackTrace();
 			return ResponseEntity.badRequest().build();
 		}
 	}
-	
+
 	@GetMapping("/image/{fileId}")
 	public ResponseEntity<?> serveFile(@PathVariable Long fileId){
 		try {
@@ -43,6 +43,6 @@ public class ImageController {
 			e.printStackTrace();
 			return ResponseEntity.badRequest().build();
 		}
-		
+
 	}
 }
--- src/main/java/com/ds/antddun/controller/JayuBoardController.java
+++ src/main/java/com/ds/antddun/controller/JayuBoardController.java
@@ -10,15 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
-@Log4j2
 @RequestMapping("/member")
-@SessionAttributes("member")
+@Log4j2
 public class JayuBoardController {
 
     @Autowired
@@ -29,14 +27,10 @@ public class JayuBoardController {
         httpSession = request.getSession();
         return "jayu/register";
     }
-
-
-    @PostMapping("/jayu/read")
+    @PostMapping("/jayu/confirm")
     public String register(JayuBoardDTO jayuBoardDTO, @AuthenticationPrincipal PrincipalDetails principal) {
-
         jayuBoardService.register(jayuBoardDTO, principal.getMember());
-        return "jayu/read";
+        return "jayu/main";
     }
 
 }
-
