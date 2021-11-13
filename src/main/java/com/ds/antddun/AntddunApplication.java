32631e2cbda685790dee8b741f54b8c145d62b84
aibeam <aibeam1025@gmail.com>
2021-11-13 12:37:43 +0900

    2021.11.13 쪽지함 레이아웃

--- src/main/java/com/ds/antddun/AntddunApplication.java
+++ src/main/java/com/ds/antddun/AntddunApplication.java
@@ -2,6 +2,7 @@ package com.ds.antddun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -11,5 +12,8 @@ public class AntddunApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AntddunApplication.class, args);
 	}
-
+	@Bean(name = "uploadPath")
+	public String uploadPath() {
+		return "C:\\\\upload";
+	}
 }
