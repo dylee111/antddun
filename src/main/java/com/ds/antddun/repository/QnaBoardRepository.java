98b7b65b896ced6e1148a0c82cf7a251f02410db
danbi-park <86641773+danbi-park@users.noreply.github.com>
2021-11-22 15:22:01 +0900

    PDB_1122
    카테고리 별 리스트 출력(페이징 안됨)

--- src/main/java/com/ds/antddun/repository/QnaBoardRepository.java
+++ src/main/java/com/ds/antddun/repository/QnaBoardRepository.java
@@ -2,11 +2,14 @@ package com.ds.antddun.repository;
 
 import com.ds.antddun.dto.QnaBoardDTO;
 import com.ds.antddun.entity.QnaBoard;
+import com.ds.antddun.entity.SosoJobBoard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface QnaBoardRepository extends JpaRepository<QnaBoard, Long> {
 
@@ -16,14 +19,24 @@ public interface QnaBoardRepository extends JpaRepository<QnaBoard, Long> {
             "GROUP BY qna.qnaNo ")
     Page<Object[]> getListPage(Pageable pageable);
 
-/*    @Query("SELECT qna FROM QnaBoard qna WHERE qna.category=:category")
-    List<QnaBoardDTO> getListByCategory(String category);*/
+
+/*    @Query(value="SELECT * FROM qna_board qna, job_list jl WHERE qna.job_list_jno = jl.jno AND qna.job_list_jno=:jno ",
+           countQuery = "SELECT count(*) FROM qna_board qna, job_list jl WHERE qna.job_list_jno = jl.jno AND qna.job_list_jno=:jno ",
+           nativeQuery = true)*/
+    @Query("SELECT qna, COUNT(likes.qnaBoard.qnaNo) " +
+            "FROM QnaBoard qna LEFT OUTER JOIN QnaLikes likes " +
+            "ON likes.qnaBoard.qnaNo = qna.qnaNo " +
+            "WHERE job_list_jno=:jno "+
+            "GROUP BY qna.qnaNo " )
+    Page<Object[]> getListByCate(int jno, Pageable pageable);
 
 
-/*
-    @Query("select qna from QnaBoard qna where qnaNo=:qnaNo")
-    List<Object[]> getBoardByQnaNo(int qnaNo);
-*/
+    @Query("SELECT qna, COUNT(likes.qnaBoard.qnaNo) " +
+            "FROM QnaBoard qna LEFT OUTER JOIN QnaLikes likes " +
+            "ON likes.qnaBoard.qnaNo = qna.qnaNo " +
+            "WHERE qna.qnaNo=:qnaNo "+
+            "GROUP BY qna.qnaNo " )
+    List<Object[]> getBoardWithAllByQnaNo(Long qnaNo);
 
 
 }
