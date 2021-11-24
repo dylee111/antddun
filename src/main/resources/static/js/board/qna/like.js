71673d66cfb7d7a6b81a62b67afa7523c9ddd13d
danbi-park <86641773+danbi-park@users.noreply.github.com>
2021-11-24 15:37:13 +0900

    PDB_1124
    - 좋아요 css구현

--- src/main/resources/static/js/board/qna/like.js
+++ src/main/resources/static/js/board/qna/like.js
@@ -3,13 +3,28 @@
     var contextPath = $('#contextPathHolder').attr('data-contextPath') ? $('#contextPathHolder').attr('data-contextPath') : '';
     var qnaNo = $('#qnaNo').text();
     var likesCnt = $('#likesCnt');
-    var likesBtn = $('#likeBtn');
+    var likeBtn = $('#likeBtn');
+    var likeBtns = $('#likeBtns');
+
+
+    $('#likeBtns').click(function(){
+        var likeBtnimg = $(this).children("span").children("img");
+
+        likeBtnimg.attr("src", function(index, attr){
+            if(attr.match('_1')){
+                return attr.replace('_1', '_0');
+            } else {
+                return attr.replace('_0', '_1');
+            }
+        });
+    });
 
     $('#likeBtn').click(function(){
             likeupdate();
         });
 
         function likeupdate(){
+
         $.ajax({
             url : contextPath + "/qna/" + qnaNo,
             type : 'POST',
@@ -18,11 +33,13 @@
             success : function(data){
                         if ( data.response == null) { //좋아요 해제 후 db에 있는 개수 반영
                             likesCnt.text(JSON.stringify(data.likesCnt));
-                            likesBtn.addClass("like_on");
+
 
 
                         } else { //좋아요, db에 있는 개수 반영
-                            likesCnt.text(JSON.stringify(data.likesCnt));
+                            likeBtn.text(JSON.stringify(data.likesCnt));
+                            likeBtns.removeAttr();
+                            likeBtns.attr('src','@{/assets/heart_1.png}');
 
                         }
             },
@@ -35,6 +52,29 @@
             });
         };
    });
+
+
+
+
+
+/*
+        $.ajax({
+            url : contextPath + "/qna/list/" + qnaNo,
+            type : 'POST',
+            data: qnaNo,
+            dataType: 'text',
+            success : function(data){
+                        if ( data == 0 ) { //좋아요 해제
+                        } else { //좋아요
+                            likesBtn.addClass("like_on");
+                        }
+            },
+            error : function(data){
+                console.log(data);
+            }
+
+        });
+*/
 
 /*
 
