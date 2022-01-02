4ece4118f1776dbaebb48e78c25dde988a14cbb6
sweg-db <bhd03067@naver.com>
2022-01-02 22:17:46 +0900

    PDB_0102
    - 조회 페이지 코드 수정

--- src/main/java/com/ds/antddun/repository/search/SearchQnaBoardRepositoryImpl.java
+++ src/main/java/com/ds/antddun/repository/search/SearchQnaBoardRepositoryImpl.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,7 +32,6 @@ public class SearchQnaBoardRepositoryImpl extends QuerydslRepositorySupport impl
         log.info("search1...");
         QQnaBoard qQnaBoard = QQnaBoard.qnaBoard;
         QQnaReply qQnaReply = QQnaReply.qnaReply;
-        QMember qMember = QMember.member;
         QQnaLikes qQnaLikes = QQnaLikes.qnaLikes;
 
         JPQLQuery<QnaBoard> jpqlQuery = from(qQnaBoard);
@@ -65,7 +65,7 @@ public class SearchQnaBoardRepositoryImpl extends QuerydslRepositorySupport impl
         jpqlQuery.leftJoin(qQnaReply).on(qQnaReply.qnaBoard.qnaNo.eq(qQnaBoard.qnaNo));
         jpqlQuery.leftJoin(qQnaLikes).on(qQnaLikes.qnaBoard.qnaNo.eq(qQnaBoard.qnaNo));
 
-        JPQLQuery<Tuple> tuple = jpqlQuery.select(qQnaBoard, qQnaReply.countDistinct(), qQnaLikes.countDistinct());
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(qQnaBoard, qQnaLikes.countDistinct(), qQnaReply.countDistinct());
 
         BooleanBuilder booleanBuilder = new BooleanBuilder(); //검색을 실행하는 객체
         BooleanExpression expression = qQnaBoard.qnaNo.gt(0L); //검색의 조건을 처리하는 객체
@@ -112,7 +112,10 @@ public class SearchQnaBoardRepositoryImpl extends QuerydslRepositorySupport impl
         Long count = tuple.fetchCount();
         log.info("COUNT: "+count);
 
-        return new PageImpl<Object[]>(result.stream().map(t->t.toArray()).collect(Collectors.toList()),pageable,count);
+        return new PageImpl<Object[]>(result.stream().map(t -> t.toArray()).collect(Collectors.toList()),
+                                      pageable,
+                                      count);
+
     }
 }
 
--- src/test/java/com/ds/antddun/repository/search/SearchQnaBoardRepositoryTests.java
+++ src/test/java/com/ds/antddun/repository/search/SearchQnaBoardRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+
 @SpringBootTest
 class SearchQnaBoardRepositoryTests {
 
