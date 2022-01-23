7e84aecf63914fa9b3e5265f8c1c99d9d22a89bd
Kimgeunhye <geunhyengineer@naver.com>
2022-01-23 17:21:49 +0900

    22.01.23 - 일반 게시글/ 펑예 목록 분리

--- src/main/java/com/ds/antddun/repository/search/SearchJayuBoardRepository.java
+++ src/main/java/com/ds/antddun/repository/search/SearchJayuBoardRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface SearchJayuBoardRepository {
+
     Page<Object[]> search(int cate, String type, String keyword, Pageable pageable);
 }
